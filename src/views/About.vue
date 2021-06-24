3e04b559d36ac3f7614ed985b4c0d8292db9786a
Viktor <viktorkatz619@gmail.com>
2021-06-24 12:49:30 +0200

    Finally inserted the map

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <GmapMap :center='mapcenter'
+                 :zoom='14'
+                 style='width:100%;  height: 400px;'>
+            <GmapMarker :key="index"
+                        :position="mapcenter"
+                        :clickable="false"
+                        :draggable="false"/>
+        </GmapMap>
+    </div>
 </template>
+
+
+<script>
+    export default {
+        name: 'About',
+        data() {
+            return {
+                mapcenter: { lat: 43.866627, lng: 20.155488 },
+            }
+        },
+    }
+
+</script>
