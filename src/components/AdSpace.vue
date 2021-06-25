d28a6d9e5323a75937acc6e2478ecfcac664aedb
Viktor <viktorkatz619@gmail.com>
2021-06-25 02:04:01 +0200

    Tactical ad order

--- src/components/AdSpace.vue
+++ src/components/AdSpace.vue
@@ -10,8 +10,8 @@
             <a href="https://www.blic.rs/">
                 <b-carousel-slide img-src="https://www.mercury-publicity.com/wp-content/uploads/2019/03/blic.jpg" />
             </a>
-            <a href="https://zena.blic.rs/">
-                <b-carousel-slide img-src="http://www.serbianlogo.com/thumbnails/blic_zena.gif" />
+            <a href="https://www.sns.org.rs/en">
+                <b-carousel-slide img-src="https://rs.n1info.com/wp-content/uploads/2020/02/slogan-302681.jpeg" />
             </a>
         </b-carousel>
         <b-carousel id="vesti-banner"
@@ -24,8 +24,8 @@
             <a href="https://www.krstarica.com/">
                 <b-carousel-slide img-src="https://play-lh.googleusercontent.com/1jKiXr9aFuKGzWOHif-gQwXymvzQ_u3EvchheI66EYoWnx0lk3HUXvDi8pKcy8nSZw" />
             </a>
-            <a href="https://rs.n1info.com/">
-                <b-carousel-slide img-src="https://www.danas.rs/wp-content/uploads/2019/07/n1-678x382.jpg" />
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                <b-carousel-slide img-src="https://neilpatel.com/wp-content/uploads/2021/02/ExamplesofSuccessfulBannerAdvertising.jpg" />
             </a>
         </b-carousel>
         <b-carousel id="third-banner"
@@ -35,11 +35,11 @@
                     fade
                     no-touch
                     class="ad-carousel mt-2">
-            <a href="https://www.sns.org.rs/en">
-                <b-carousel-slide img-src="https://rs.n1info.com/wp-content/uploads/2020/02/slogan-302681.jpeg" />
+            <a href="https://rs.n1info.com/">
+                <b-carousel-slide img-src="https://www.danas.rs/wp-content/uploads/2019/07/n1-678x382.jpg" />
             </a>
-            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
-                <b-carousel-slide img-src="https://neilpatel.com/wp-content/uploads/2021/02/ExamplesofSuccessfulBannerAdvertising.jpg" />
+            <a href="https://zena.blic.rs/">
+                <b-carousel-slide img-src="http://www.serbianlogo.com/thumbnails/blic_zena.gif" />
             </a>
         </b-carousel>
     </div>
