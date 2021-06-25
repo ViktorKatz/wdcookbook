36c794d894fefff48178cc0d04c30ffc282977df
Viktor <viktorkatz619@gmail.com>
2021-06-25 16:25:41 +0200

    Hardcoded some blog posts

--- src/data/blogposts.js
+++ src/data/blogposts.js
@@ -1,11 +1,30 @@
-const blogposts = [
+﻿const blogposts = [
     {
         id: 1,
-        author: 'Viktor',
-        title: 'asd',
-        text: 'asd',
-        img: '',
-        date: '',
+        author: 'Marina',
+        title: 'Kuvanje sa decom - zanimljiva i jeftina aktivnost',
+        text: 'Kuhinja je pravo magično mesto gde se razno povrće pretvara u ukusan ručak a brašno i jaja u divan kolač. Iskoristite znatiželju vašeg deteta i organizujte zajedničke trenutke u kuhinji da biste približili najmlađe članove porodice zdravoj ishrani. Svako dete poželi da se priključi mami, baki ili nekom drugom članu porodice u kuhinji kako bi napravilo nešto ukusno.Kuhinja je pravo magično mesto gde se razno povrće pretvara u ukusan ručak a brašno i jaja u divan kolač.Iskoristite znatiželju vašeg deteta i organizujte zajedničke trenutke u kuhinji da biste približili najmlađe članove porodice zdravoj ishrani.',
+        img: 'https://api.najboljamamanasvetu.com/uploads/2010/08/Kuvanje-zajedno.jpg',
+        date: '22.6.2021.',
+        linkmore: 'https://najboljamamanasvetu.com/kuvanje-sa-decom-zanimljiva-i-jeftina-aktivnost/5d3adca5fc73c718d4bac755'
+    },
+    {
+        id: 2,
+        author: 'Kuvar Steva',
+        title: '20 genijalnih trikova poznatih kuvara',
+        text: 'Ako želite da dodate lagane note belog luka u jelo, ali se plašite da ne preterate sa mirisom, pre nego što stavite hranu, tiganj istrljajte čenom belog luka. Iskusni kuvari su pronašli dostojnu primenu za pivo: ovo piće sa soja sosom biće odlična marinada za meso, a mala količina tamnog piva koja se doda prilikom grilovanja povrća učiniće ga još ukusnijim. Ako preterate sa količinom soli dok kuvate supu, ne očajavajte.Stavite malu količinu neke žitarice u gazu i umočite je u supu.Dok se kuva, žitarice će apsorbovati suvišnu so. Kako svinjsko meso ne bi bilo tvrdo, posolite ga tek na kraju pečenja. Da bi se beli pirinač brže skuvao, dodajte nekoliko kapi sirćeta u vodu. Da se žumance ne bi raspalo dok sečete kuvano jaje, umočite nož u hladnu vodu pre toga.',
+        img: 'https://stil.kurir.rs/data/images/2018/04/28/13/153579_profimedia0187807267_ls.jpg',
+        date: '11.06.2021.',
+        linkmore: 'https://stil.kurir.rs/trpeza/hrana-vino/111567/20-genijalnih-trikova-poznatih-kuvara-jedan-ce-vas-posebno-iznenaditi'
+    },
+    {
+        id: 3,
+        author: 'Društvo paprikašara Srbije',
+        title: 'Takmičenje u pripremanju riblje čorbe i ribljeg paprikaša',
+        text: 'U nedelju 3. avgusta, poslednjeg dana Fish Fest-a, organizujemo dva takmičenja: pripremanje riblje čorbe i ribljeg paprikaša. Za tri najbolja kuvara iz obe discipline pripremamo novčane nagrade, dok će sponzori obezbediti robne nagrade za takmičare od 4. do 10. mesta.',
+        img: 'https://www.ravnoplov.rs/wp-content/uploads/2018/06/R5K_1050.jpg',
+        date: '29.05.2021.',
+        linkmore: 'https://fishfestbeograd.rs/2020/07/10/takmicenje-u-pripremanju-riblje-corbe-i-ribljeg-paprikasa/'
     },
 ];
 
