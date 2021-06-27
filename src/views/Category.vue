9a37e350367ad9b360559e2ff6bd4ffd89412315
gavantee <milovancevicdragan@gmx.com>
2021-06-27 18:09:03 +0200

    search u kategoriji

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,18 +1,30 @@
 <template>
     <div>
-        <h1>{{$t(categories[this.id - 1].i18n_name)}}</h1>
-        <div style="text-align: left;">
-            {{$t('categories.sort')}}:
-            <b-form-select v-model="sort" @change="resort">
-                <option value="0">{{$t("categories.order.id")}} {{$t("categories.order.asc")}}</option>
-                <option value="1">{{$t("categories.order.id")}} {{$t("categories.order.desc")}}</option>
-                <option value="2">{{$t("categories.order.rating")}} {{$t("categories.order.asc")}}</option>
-                <option value="3">{{$t("categories.order.rating")}} {{$t("categories.order.desc")}}</option>
-                <option value="4">{{$t("categories.order.difficulty")}} {{$t("categories.order.asc")}}</option>
-                <option value="5">{{$t("categories.order.difficulty")}} {{$t("categories.order.desc")}}</option>
-            </b-form-select>
-        </div>
-        <RecipeCard v-for="recipe in this.recipes"
+        <b-row>
+            <h1 style="padding-bottom: 20px;">{{$t(categories[this.id - 1].i18n_name)}}</h1>
+            <b-col cols="12" lg="6">
+                <div style="text-align: left;">
+                    {{$t('categories.sort')}}:
+                    <b-form-select v-model="sort" @change="resort">
+                        <option value="0">{{$t("categories.order.id")}} {{$t("categories.order.asc")}}</option>
+                        <option value="1">{{$t("categories.order.id")}} {{$t("categories.order.desc")}}</option>
+                        <option value="2">{{$t("categories.order.rating")}} {{$t("categories.order.asc")}}</option>
+                        <option value="3">{{$t("categories.order.rating")}} {{$t("categories.order.desc")}}</option>
+                        <option value="4">{{$t("categories.order.difficulty")}} {{$t("categories.order.asc")}}</option>
+                        <option value="5">{{$t("categories.order.difficulty")}} {{$t("categories.order.desc")}}</option>
+                    </b-form-select>
+                </div>
+            </b-col>
+            <b-col cols="12" lg="6">
+                <b-form @submit="doSearch">
+                    <div class="flex-row">
+                        <b-form-input v-model="search" v-bind:placeholder="$t('header.search')"></b-form-input>
+                        <b-button type="sumbit"> <b-icon-search /> </b-button>
+                    </div>
+                </b-form>
+            </b-col>
+        </b-row>
+        <RecipeCard v-for="recipe in this.searchRecipes"
                     :key="recipe.id"
                     :title="recipe.title"
                     :difficulty="recipe.difficulty"
@@ -27,6 +39,10 @@
 </template>
 
 <style scoped>
+    .flex-row {
+        display: flex;
+        flex-direction: row;
+    }
     h3 {
         padding-bottom: 20px;
     }
@@ -66,12 +82,15 @@
                     (a, b) => this.average(b.ratings) - this.average(a.ratings),
                     (a, b) => a.difficulty - b.difficulty,
                     (a, b) => b.difficulty - a.difficulty,
-                ]
+                ],
+                search: "",
+                searchRecipes: []
             }
         },
         created() {
             this.$title = this.$t(categories[this.id - 1].i18n_name);
             this.recipes = JSON.parse(localStorage.getItem('recipes')).filter(e => e.category == this.id);
+            this.searchRecipes = this.recipes;
         },
         methods: {
             average(array) {
@@ -85,11 +104,17 @@
             },
             resort(val) {
                 this.recipes = this.recipes.sort(this.sortfs[val]);
+            },
+            doSearch(e) {
+                e.preventDefault();
+                this.searchRecipes = this.recipes.filter(e => e.title.toLowerCase().search(this.search) != -1);
             }
         },
         beforeRouteUpdate(to, from, next) {
             this.id = to.params.id;
             this.recipes = JSON.parse(localStorage.getItem('recipes')).filter(e => e.category == this.id);
+            this.searchRecipes = this.recipes;
+            this.search = "";
             this.sort = 0;
             this.resort(this.sort);
             next();
